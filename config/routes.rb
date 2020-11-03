6e36434be70d46c99b5a6645923408d9139c098f
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-03 14:43:37 +0900

    新規登録画面

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/', to: 'users#index'
   get '/communities', to: 'communities#index'
   get '/communities/1', to: 'communities#show'
+  get '/users/new', to: 'users#new'
 end
